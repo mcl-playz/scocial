6317702a040c38e95a2cc830cea7d4acb90c0853
mcl-playz <elvi.green11@gmail.com>
2023-08-29 14:14:19 +0800

    initial commit

---
+++ code.js
@@ -0,0 +1,12 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("menuContainer").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("menuContainer").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
